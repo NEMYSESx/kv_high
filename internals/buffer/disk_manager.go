c0f9dd2f75eb9eab420e7632ce4ffe3988430f65
NEMYSESx <shakirraza4227@gmail.com>
2025-01-29 12:42:01 +0530

    buffer_pool internal implementation completed

--- internals/buffer/disk_manager.go
+++ internals/buffer/disk_manager.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 const DiskMaxNumPages = 15
 
 type DiskManager interface {
@@ -7,4 +11,39 @@ type DiskManager interface {
 	WritePage(*Page) error
 	AllocatePage() *PageID
 	DeallocatePage(PageID)
+}
+
+type DiskManagerMock struct {
+	numPage int 
+	pages   map[PageID]*Page
+}
+
+func (d *DiskManagerMock) ReadPage(pageID PageID) (*Page, error) {
+	if page, ok := d.pages[pageID]; ok {
+		return page, nil
+	}
+
+	return nil, errors.New("Page not found")
+}
+
+func (d *DiskManagerMock) WritePage(page *Page) error {
+	d.pages[page.id] = page
+	return nil
+}
+
+func (d *DiskManagerMock) AllocatePage() *PageID {
+	if d.numPage == DiskMaxNumPages-1 {
+		return nil
+	}
+	d.numPage = d.numPage + 1
+	pageID := PageID(d.numPage)
+	return &pageID
+}
+
+func (d *DiskManagerMock) DeallocatePage(pageID PageID) {
+	delete(d.pages, pageID)
+}
+
+func NewDiskManagerMock() *DiskManagerMock {
+	return &DiskManagerMock{-1, make(map[PageID]*Page)}
 }
