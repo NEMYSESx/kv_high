9c8946fcf4ad0a41ef98941e204ed963780efe40
NEMYSESx <shakirraza4227@gmail.com>
2025-02-02 23:41:06 +0530

    made log file

--- internals/storage/log.go
+++ internals/storage/log.go
@@ -3,23 +3,36 @@ package main
 import (
 	"fmt"
 	"hash/crc32"
+	"os"
 )
 
 type Record struct {
-    CRC       uint32
-    Timestamp uint32  
-    KeySize   uint32
-    ValueSize uint32
-    Key       string
-    Value     string
+	CRC       uint32
+	Timestamp uint32  
+	KeySize   uint32
+	ValueSize uint32
+	Key       string
+	Value     string
 }
 
-func main(){
-	data := []byte("Hello world!")
+func main() {
+	file, err := os.OpenFile("append.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening the file:", err)
+		return
+	}
+	defer file.Close()
 
-	fmt.Println(data)
+	_, err = file.WriteString("New log entry\n")
+	if err != nil {
+		fmt.Println("Error writing to the file:", err)
+		return
+	}
+	fmt.Println("File written successfully!")
+
+	data := []byte("Hello world!")
+	fmt.Println("Data:", string(data))
 
 	checkSum := crc32.ChecksumIEEE(data)
-
 	fmt.Printf("CRC-32 (IEEE): %08x\n", checkSum)
 }
